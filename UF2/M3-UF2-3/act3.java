3af884c21fabbb130ac0c82b7a3ee53aa67621e1
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-02-14 17:18:33 +0100

    master

--- UF2/M3-UF2-3/act3.java
+++ UF2/M3-UF2-3/act3.java
@@ -2,17 +2,18 @@ import java.io.*;
  
 public class act3 {
 
-    public static int [] nums(int a, int b,int c, int d){
+    public static int nums(int a, int b,int c, int d){
         int[] myArray = {a*b, a*c, a*d, d*b, d*c, c*b};
-        return();
-    }
-
-
-    public static int numeros(int a, int b, int c, int d){
-        for (int i=0;i<3;i++){
-            int[] myArray = {a*b, a*c, a*d, d*b, d*c, c*b};
-            System.out.print(myArray);
+        int hola=0;
+        for (int j=0;j<4;j++){
+            if (j==0){
+                hola = myArray[0];
+            }
+            else if (myArray[j]>hola){
+                hola = myArray[j];
+            }
         }
+        return hola;
     }
 
     public static void main(String[] args) throws IOException {
@@ -20,16 +21,17 @@ public class act3 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         //######DEFINO LAS VARIABLES ######
-        int array[] = new int[3];
+        int array[] = new int[4];
         //#################################
 
-        System.out.print( "============ SUMA HORA MINUTOS Y SEGUNDOS ============= \n");
+        System.out.print( "============ EL NUMERO MAYOR :) ============= \n");
 
         for (int i=0;i<4;i++){
-            System.out.print("Ingrese un numero"+ (i+1) +": ");
+            System.out.print("Ingrese un numero "+ (i+1) +": ");
 			String nota1 = reader.readLine();
-			array[i] = Integer.parseInt(nota1);
+            array[i] = Integer.parseInt(nota1);
         }
-
+        int benito = nums(array[0],array[1],array[2],array[3]);
+        System.out.println("El maximo es: "+benito);
     }
 }
