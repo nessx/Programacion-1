d3bff5211a32a4a85fd1e1754c62a92ea616a876
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-04-12 06:09:16 +0200

    master

---
+++ UF3/manejo de archivos/act5.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+print "#####################"
+print "##### TEST FILE #####"
+print "#####################\n"
+
+file = raw_input("Donde esta el archivo?: ")
+
+try:
+    archivo = open(file, "r")
+    print "El archivo contiene: \n", (archivo.read())
+
+except IOError:
+    print("ERROR: El archivo no es accesible o no existe!")
+else:
+    archivo.close()
