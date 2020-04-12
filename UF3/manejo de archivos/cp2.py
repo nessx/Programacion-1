2b982b2351100b04329638b6d942a74567946dc8
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-04-12 05:31:33 +0200

    master

---
+++ UF3/manejo de archivos/cp2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+archivo = open("test.txt", "r")
+
+fil = "demofile.txt"
+
+#abrir el nuevo archivo y escribir lo que hay en el anterior
+f = open(fil, "a")
+f.write(archivo.readline())
+f.close()
+
+#abrir y leer el archivo
+f = open(fil, "r")
+print(f.read(5))
