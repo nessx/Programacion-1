02bb994cc83ced5ecd27ffada936c358c7d5056c
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-04-16 17:27:02 +0200

    master

--- UF3/projecte1/mainex.py
+++ UF3/projecte1/mainex.py
@@ -1,12 +1,9 @@
 import csv
  
-with open('Crushers.csv') as File:
+with open('01001c.csv') as File:
     reader = csv.reader(File, delimiter=',')
     for row in reader:
 		lista = row[0]
 		lista1 = row[1]
-		lista2 = row[2]
-		lista3 = row[3]
-		lista4 = row[4]
-		lista5 = row[5]
-		print lista,",",lista1,",",lista2,",",lista4,",",lista5
+		#lista2 = row[2]
+		print lista,"|",lista1,"|"
