a7832d912227a8e8efed8601f95ba959d220674d
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-04-16 19:26:58 +0200

    master

--- UF3/projecte1/mainex.py
+++ UF3/projecte1/mainex.py
@@ -5,5 +5,9 @@ with open('01001c.csv') as File:
     for row in reader:
 		lista = row[0]
 		lista1 = row[1]
-		#lista2 = row[2]
-		print lista,"|",lista1,"|"
+		lista2 = row[2]
+		lista3 = row[3]
+		lista4 = row[4]
+		lista5 = row[5]
+		lista6 = row[6]
+		print lista,"|",lista1,"|",lista2,"|",lista3,"|",lista4,"|",lista5,"|",lista6,"|"
