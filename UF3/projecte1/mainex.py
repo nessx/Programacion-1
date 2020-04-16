f55469aa5967d00461214e0e4935d16112876cb3
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-04-17 01:37:55 +0200

    master

--- UF3/projecte1/mainex.py
+++ UF3/projecte1/mainex.py
@@ -1,14 +1,39 @@
 import csv
- #!/usr/bin/python
 
 print "#####################"
 print "## CSV A HTML TABLA##"
 print "#####################\n"
 
 try:
+	archivo2 = open("index.html","w+")
+
+	archivo2.write("<html>\n")
+	archivo2.write("<head>\n")
+
+	archivo2.write("<style>\n")
+	archivo2.write("table, th, td {")
+	archivo2.write("border: 1px solid black;")
+	archivo2.write("}")
+	archivo2.write("</style>\n")
+
+	archivo2.write("<body>\n")
+	archivo2.write("<table>\n")
+	archivo2.write("<tr>\n")
+	archivo2.write("<th> years </th>\n")
+	archivo2.write("<th> dato1 </th>\n")
+	archivo2.write("<th> dato2 </th>\n")
+	archivo2.write("<th> dato3 </th>\n")
+	archivo2.write("<th> dato4 </th>\n")
+	archivo2.write("<th> dato5 </th>\n")
+	archivo2.write("<th> dato6 </th>\n")
+	archivo2.write("</tr>\n")
+	archivo2.write("<tr>\n")
+
+	listta = []
 	with open('01001c.csv') as archivo:
 		lector = csv.reader(archivo, delimiter=',')
 		for row in lector:
+
 			lista = row[0]
 			lista1 = row[1]
 			lista2 = row[2]
@@ -16,33 +41,41 @@ try:
 			lista4 = row[4]
 			lista5 = row[5]
 			lista6 = row[6]
-			print lista,"|",lista1,"|",lista2,"|",lista3,"|",lista4,"|",lista5,"|",lista6,"|"
 
-	archivo2 = open("index.html","w+")
+			archivo2.write("<tr>\n")
+			
+			archivo2.write("\t<td>")
+			archivo2.write(lista)
+			archivo2.write("\t</td>\n")
 
-	archivo2.write("<html>\n")
-	archivo2.write("<head>\n")
-	archivo2.write("<body>\n")
-	archivo2.write("<table>\n")
-	archivo2.write("<tr>\n")
-	archivo2.write("<td> years </td>\n")
-	archivo2.write("<td> dato1 </td>\n")
-	archivo2.write("<td> dato2 </td>\n")
-	archivo2.write("<td> dato3 </td>\n")
-	archivo2.write("<td> dato4 </td>\n")
-	archivo2.write("<td> dato5 </td>\n")
-	archivo2.write("<td> dato6 </td>\n")
-	archivo2.write("</tr>\n")
-	archivo2.write("<tr>\n")
+			archivo2.write("\t<td>")
+			archivo2.write(lista1)
+			archivo2.write("\t</td>\n")
 
-	for row in archivo:
-		archivo2.write("<td>",lista,"</td>\n")
-		archivo2.write("<td>",lista1,"</td>\n")
-		archivo2.write("<td>",lista2,"</td>\n")
-		archivo2.write("<td>",lista3,"</td>\n")
-		archivo2.write("<td>",lista4,"</td>\n")
-		archivo2.write("<td>",lista5,"</td>\n")
-		archivo2.write("<td>",lista6,"</td>\n")
+			archivo2.write("\t<td>")
+			archivo2.write(lista2)
+			archivo2.write("\t</td>\n")
+
+			archivo2.write("\t<td>")
+			archivo2.write(lista3)
+			archivo2.write("\t</td>\n")
+
+			archivo2.write("\t<td>")
+			archivo2.write(lista4)
+			archivo2.write("\t</td>\n")
+
+			archivo2.write("\t<td>")
+			archivo2.write(lista5)
+			archivo2.write("\t</td>\n")
+
+			archivo2.write("\t<td>")
+			archivo2.write(lista6)
+			archivo2.write("\t</td>\n")
+
+			archivo2.write("</tr>\n")
+
+			#print lista,"|",lista1,"|",lista2,"|",lista3,"|",lista4,"|",lista5,"|",lista6,"|"
+
 
 	archivo2.write("</tr>\n")
 	archivo2.write("</table>\n")
@@ -50,12 +83,15 @@ try:
 	archivo2.write("</head>\n")
 	archivo2.write("</html>\n")
 	archivo2.close()
-		
 
 except IOError:
     print("ERROR: El archivo no es accesible o no existe!")
+except IndexError:
+    print("Error, en su archivo csv, por favor revise que el fichero este escrito correctamente!, -> error (IndexError: list index out of range)")
 except ValueError:
     print("ERROR: I/O operacion en un archivo cerrado, revisa que no cierras el archivo antes de leer las lineas que necesites.")
+except TypeError:
+    print("ERROR")
 else:
     archivo.close()
 
--- UF4/GatPersa.java
+++ UF4/GatPersa.java
@@ -1,8 +1,46 @@
+/*
 public class GatPersa {
     int vides = 7;   // vides disponibles del gat Persa
     public static void main(String[] args) {
         GatPersa renat;           // declarem la referència al gat
         renat = new GatPersa();   // creem la instància del gat Persa.
         System.out.println("Al gat Renat li queden " + renat.vides + " vides");
+    }
+}
+*/
+
+/*public class GatPersa {
+    int vides = -12;   // vides disponibles del gat Persa
+    public static void main(String[] args) {
+        GatPersa renat;           // declarem l'objecte (la referència) al gat
+        renat = new GatPersa();   // creem la instància del gat Renat.
+        System.out.println("Abans el gat Renat tenia " + renat.vides + " vides");
+        //renat.vides = renat.vides - 1;  // Renat ha tingut un accident
+        System.out.println("Ara el gat Renat té " + renat.vides);
+    }
+}*/
+
+public class GatPersa {
+    int vides = 7;   // vides disponibles del gat Persa
+ 
+    public static void main(String[] args) {
+        GatPersa renat_I;
+        renat_I = new GatPersa();               // crea un primer gat Persa
+        System.out.println("El gat Renat I té  " + renat_I.vides + " vides");
+ 
+        GatPersa renat_II = new GatPersa();     // crea un segon gat Persa!
+        System.out.println("El gat Renat II té " + renat_II.vides + " vides");
+ 
+        renat_I.vides++;
+        System.out.println("El gat Renat I té  " + renat_I.vides + " vides");
+        System.out.println("El gat Renat II té " + renat_II.vides + " vides");
+ 
+        renat_II = renat_I; // atenció: la instància de Renat II es perd!
+        System.out.println("El gat Renat I té  " + renat_I.vides + " vides");
+        System.out.println("El gat Renat II té " + renat_II.vides + " vides");
+ 
+        renat_I.vides++;
+        System.out.println("El gat Renat I té  " + renat_I.vides + " vides");
+        System.out.println("El gat Renat II té " + renat_II.vides + " vides");
     }
 }
