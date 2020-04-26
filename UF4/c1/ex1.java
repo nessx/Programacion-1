b927fbece40791041050460f259649bb8aafd7cd
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-04-26 17:10:00 +0200

    m

--- UF4/c1/ex1.java
+++ UF4/c1/ex1.java
@@ -1,6 +1,7 @@
 package c1;
 
 import java.io.*;
+import java.util.concurrent.TimeUnit;
 
 public class ex1 {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -26,6 +27,7 @@ public class ex1 {
         while(reloj1.getfin()){
             System.out.println(reloj1.toString());
             reloj1.incrementa();
+            //TimeUnit.SECONDS.sleep(1);
         }
     }
 
