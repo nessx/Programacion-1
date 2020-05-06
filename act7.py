6b943845763423d624a60b5f1ffdb6ba7b8f0fe1
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-05-07 01:42:10 +0200

    hhh

--- act7.py
+++ act7.py
@@ -19,7 +19,7 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-
+kjk
 nota=[]
 nalumnos = int(raw_input("Cuantos alumnos quieres introducir?: "))
 
