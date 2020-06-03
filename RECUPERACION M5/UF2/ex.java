9e0757d56e1c2235eb6cb092366c7332a2415a6e
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-06-03 23:50:56 +0200

    master

---
+++ RECUPERACION M5/UF2/ex.java
@@ -0,0 +1,106 @@
+package UF2;
+
+import java.io.*;
+
+ 
+public class ex{
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static int i,contador=0;
+
+  public static void main(String[] args) throws IOException {
+      char [] palabra=new char[15];
+
+      palabra=getpalabra();
+      compararcaracteres(palabra);
+    }
+
+    //funcion encargada de preguntar la palabra y retornarla
+    public static char[] getpalabra() throws IOException{
+        System.out.printf("Diguem una paraula?: ");
+
+        return reader.readLine().toCharArray();
+    }
+
+    //es la funcion encargada de comparar los caracteres
+    public static void compararcaracteres (char [] palabra){
+        for (i = 0 ;i < palabra.length ; i++){
+            char word = palabra[i]; 
+            switch(word){ 
+                case 'a':
+                    contador++;
+                    verinfo(palabra[i], palabra, contador);
+                    break; 
+                case 'e':
+                    contador++;
+                    verinfo(palabra[i], palabra, contador); 
+                    break; 
+                case 'i':
+                    contador++;
+                    verinfo(palabra[i], palabra, contador); 
+                    break;
+                case 'o': 
+                    contador++;
+                    verinfo(palabra[i], palabra, contador); 
+                    break;
+                case 'u':
+                    contador++;
+                    verinfo(palabra[i], palabra, contador); 
+                    break;
+                default: 
+                    System.out.print("Vocal no valida"); 
+            }
+            
+            //esta serie de if funciona correctamente, pero quiero lograr con switch lo que hace con if
+
+            /*
+            if (palabra[i]=='a'){
+                contador++;
+                if (contador>=3){    
+                    verinfo(palabra[i], palabra, contador);
+                    break;
+                }
+            }
+            if (palabra[i]=='e'){
+                contador++;
+                if (contador>=3){    
+                    verinfo(palabra[i], palabra, contador);
+                   break;
+               }
+            }
+            if (palabra[i]=='i'){
+                contador++;
+                if(contador>=3){
+                    verinfo(palabra[i], palabra, contador);
+                break;
+                }
+            }
+            if (palabra[i]=='o'){
+                contador++;
+                if(contador>=3){
+                    verinfo(palabra[i], palabra, contador);
+                break;
+                }
+            }
+            if (palabra[i]=='u'){
+                contador++;
+                if(contador>=3){
+                    verinfo(palabra[i], palabra, contador);
+                break;
+                }
+            }
+            */
+        }
+    }
+
+    //esta solo hace de printer
+    public static void verinfo(char letra, char[] palabra, int contador){
+        //System.out.printf("el nom de " + letra + " " + palabra +" "+ contador);
+
+        System.out.println("el nom de "+ palabra + " conte " + contador + " " +letra);
+
+        //System.out.printf("el nom de "+ palabra + " conte " + contador + " " +letra);
+        //System.out.println(contador);
+        //System.out.println(letra);
+        //System.out.println(palabra);
+    }
+}
