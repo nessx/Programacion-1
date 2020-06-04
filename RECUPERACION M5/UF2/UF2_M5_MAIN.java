fd63507c28fbb35ea01d58c30af95723f72c9abe
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-06-04 23:18:31 +0200

    todo ok

--- RECUPERACION M5/UF2/UF2_M5_MAIN.java
+++ RECUPERACION M5/UF2/UF2_M5_MAIN.java
@@ -3,113 +3,106 @@ package UF2;
 import java.io.*;
 
 /**
-
  *Este programa lo que hace es recibir una entrada de una palabra y devuelve una cuenta de cuantas vocales tiene
-
  * @author: Nestor Santana
-
  * @version: V1.0
-
+ * @see <a href="https://github.com/nessx/Programacion-1/tree/master/RECUPERACION%20M5/UF2">GitHub</a>
  */
  
 public class UF2_M5_MAIN{
-    //variables única para todas las instancias (objetos) de la clase (ocupa un único lugar en memoria)
     static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    static int i,contador=0;
+    static int i, acontador=0, econtador = 0, icontador = 0, ocontador = 0, ucontador = 0, counter = 0 ;
 
-  public static void main(String[] args) throws IOException {
-      char [] palabra=new char[15];
+    public static void main(String[] args) throws IOException {
+        char [] palabra=new char[15];
+      
+        //le asigno a palabra lo que recibe de la funcion que pregunta en este caso getpalabra()  
+        palabra=getpalabra();
 
-      //le asigno a palabra lo que recibe de la funcion que pregunta en este caso getpalabra()  
-      palabra=getpalabra();
-
-      //envia todo lo que es palabra a compararcaracteres
-      compararcaracteres(palabra);
+        //envia todo lo que es palabra a compararcaracteres
+        compararcaracteres(palabra);
     }
-
-    //funcion encargada de preguntar la palabra y retornarla a la array de caracteres
      /**
-
-     * Método que devuelve el número de ítems (números aleatorios) existentes en la serie
-
+     * Metodo que devuelve la palabra introducida por el usuario
      * @return La palabra introducida por el usuario.
-
      */
+    //funcion encargada de preguntar la palabra y retornarla a la array de caracteres
     public static char[] getpalabra() throws IOException{
         System.out.printf("Diguem una paraula?: ");
         return reader.readLine().toCharArray();
     }
-
-    //es la funcion encargada de comparar los caracteres
     /**
-     * Constructor para la serie de números aleatorios
-
+     * Metodo que compara los caracteres recibidos por del metodo getpalabra
      * @param palabra recibe toda la informacion dentro de la array de caracteres
-
      */
+    //es la funcion encargada de comparar los caracteres
     public static void compararcaracteres (char [] palabra){
         try {
-            for (i = 0 ;i < palabra.length ; i++){
+            for (int i = 0 ;i < palabra.length ; i++){
                 if (palabra[i]=='a'){
-                    contador++;
-                    if (contador>1){    
-                        verinfo(palabra[i], palabra, contador);
-                        break;
+                    acontador++;
+                    if (acontador>=1 && i == palabra.length-1){  
+                        counter++;  
+                        result(palabra[i], palabra, acontador, counter);
                     }
                 }
                 if (palabra[i]=='e'){
-                    contador++;
-                    if (contador>1){    
-                        verinfo(palabra[i], palabra, contador);
-                        break;
+                    econtador++;
+                    if (econtador>=1){   
+                        counter++;   
+                        result(palabra[i], palabra, econtador, counter);
                     }
                 }
                 if (palabra[i]=='i'){
-                    contador++;
-                    if(contador>1){
-                        verinfo(palabra[i], palabra, contador);
-                        break;
+                    icontador++;
+                    if(icontador>=1){
+                        counter++;  
+                        result(palabra[i], palabra, icontador, counter);
                     }
                 }
                 if (palabra[i]=='o'){
-                    contador++;
-                    if(contador>1){
-                        verinfo(palabra[i], palabra, contador);
-                        break;
+                    ocontador++;
+                    if(ocontador>=1){
+                        counter++;  
+                        result(palabra[i], palabra, ocontador, counter);
                     }
                 }
                 if (palabra[i]=='u'){
-                    contador++;
-                    if(contador>1){
-                        verinfo(palabra[i], palabra, contador);
-                        break;
+                    ucontador++;
+                    if(ucontador>=1){
+                        counter++;  
+                        result(palabra[i], palabra, ucontador, counter);
                     }
                 }
             }
-        } catch (Exception e) {
+        }catch (Exception e) {
             System.out.println("Algo funciona mal...");
         }
     }
-    
-    //esta solo hace de printer
     /**
-    * Constructor para la serie de números aleatorios
+    * Metodo que imprime la comparacion
     
-    * @param letra Recive la letra introducida
-
+    * @param letra Recibe la letra introducida
     * @param palabra Recibe toda la informacion dentro de la array de caracteres
-
-    * @param contador Recibe el contador
-
+    * @param count Recibe un contador
+    * @param counter Recibe un contador
     */
-    public static void verinfo(char letra, char[] palabra, int contador){
+    //esta solo hace de printer
+    public static void result(char letra, char[] palabra, int count, int counter){
         try {
-            System.out.print("el nom de ");
-            for (i = 0 ;i < palabra.length ; i++){
+            if (counter == 1) {
+                System.out.print("La palabra");
+            }
+            System.out.print(" ");
+            for (int i = 0 ;i < palabra.length ; i++){
                 System.out.print(palabra[i]); 
             }
-            System.out.print(" conte " + contador + " " +letra);
-        } catch (Exception e) {
+            if (counter > 1) {
+                System.out.println(" tiene " +count + " "+letra);
+            }else{
+                System.out.println(" tiene " +count + " "+letra);
+            }
+        }catch (Exception e) {
             System.out.println("Algo funciona mal con la impresion...");
         }
     }
