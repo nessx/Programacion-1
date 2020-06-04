338bfedc1d68971bbd13e4c5204f025ce9be3570
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-06-04 17:28:44 +0200

    master

---
+++ RECUPERACION M5/UF2/M5_UF2_RECUPERATIONTest.java
@@ -0,0 +1,17 @@
+package default;
+import org.junit.Test;
+import org.junit.Before;    
+    
+public class M5_UF2_RECUPERATIONTest {
+
+    @Before
+    public void setup(){
+
+    }
+        
+    @Test
+    public void test() {
+        
+    }
+}
+    
